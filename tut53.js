95aae6f9bc35da88555777495fc73ec48197a64f
Sameer Ahmed <sameerpk58@gmail.com>
2021-01-04 21:57:48 +0530

    Add files via upload
    1st js file

---
+++ tut53.js
@@ -0,0 +1,14 @@
+console.log("this is java script")
+
+function greet(name){
+   console.log(name +  "is a good boy")
+}
+
+let name="sam";
+let name1="sjam";
+let name2="swedeam";
+let name3="sdq3fam";
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
